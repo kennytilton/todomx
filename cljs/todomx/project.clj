173b527dbc9aa5eb9d815d448548f13f2bfc8194
Kenneth Tilton <kentilton@gmail.com>
2018-01-09 15:52:19 -0500

    todomx runs ok with checkouts

--- cljs/todomx/project.clj
+++ cljs/todomx/project.clj
@@ -1,8 +1,19 @@
 (defproject todomx "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "The TodoMVC Classic implemented in Tag, the web un-framework for the Matrix (datflow library)"
+  :url "https://github.com/kennytilton"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/clojurescript "1.9.671"]
+                 [com.cognitect/transit-cljs "0.8.243"]
+                 [com.andrewmcveigh/cljs-time "0.5.2"]
+                 [funcool/bide "1.5.0"]
+                 [clj-http "3.7.0"]
+                 [cljs-http "0.1.44"]
+                 [cheshire "5.8.0"]
+                 [com.taoensso/tufte "1.1.2"]
+                 [com.tiltontec/matrix "0.1.2-SNAPSHOT"]
+                 [com.tiltontec/xhr "0.1.1-SNAPSHOT"]
+                 [com.tiltontec/tag "0.1.0-SNAPSHOT"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
--- cljs/todomx/src/todomx/core.cljs
+++ cljs/todomx/src/todomx/core.cljs
@@ -1,9 +1,28 @@
 (ns todomx.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require
+    [clojure.browser.repl :as repl]
+    [goog.dom :as dom]
+    [tiltontec.model.core :as md]
+    [tiltontec.tag.html :refer [tag-dom-create *tag-trace*]]
+    [todomx.todomx :as tmx]
+    [taoensso.tufte :as tufte :refer (defnp p profiled profile)]
+    [cljs-time.coerce :refer [from-long to-string] :as tmc]))
 
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
 
 (enable-console-print!)
 
-(println "Hello world!")
+(tufte/add-basic-println-handler! {})
+
+(let [root (dom/getElement "tagroot")
+      app-matrix (tmx/matrix-build!)
+      app-dom (binding [*tag-trace* nil]                ;; <-- set to nil if console too noisy
+                (tag-dom-create
+                  (md/md-get app-matrix :mx-dom)))]
+
+  (set! (.-innerHTML root) nil)
+  (dom/appendChild root app-dom)
+  (when-let [route-starter (md/md-get app-matrix :router-starter)]
+    (route-starter)))
+
